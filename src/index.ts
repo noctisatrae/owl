9eb4ede41f31126b1bd560ffe10255aa30751439
noctisatrae <pro.hector.kub@gmail.com>
2022-06-24 21:55:22 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import p from "https://esm.sh/phin";
 import { parse } from "https://esm.sh/toml"
 
+const required: string[] = ["name", "version", "description", "author", "script"]
+
 async function load(url:string) {
     const pkg = await p({
         url: url,
@@ -9,17 +11,49 @@ async function load(url:string) {
     
     const owl = parse(pkg.body);
 
-    // Information
+    /*
+
+    Information
     const name: string = owl["Information"].name
     const version: string = owl["Information"].version
     const description: string = owl["Information"].description
     const homepage: string = owl["Information"].homepage
 
-    // Data
+    Data
     const files: string = owl["Data"].files
     const script: string = owl["Data"].script
     const checksum: string = owl["Data"].checksum
 
-    return {info:[name, version, description, homepage], data:[]};
+
+    return {info:[name, version, description, homepage], data:[files, script, checksum]};
+
+    */
+
+    const info = Object.entries(owl["Information"]);
+    const data = Object.entries(owl["Data"]);
+
+    let available: string[] = [];
+
+    for(let i = 0; i < info.length; i++) {
+        let [key, value] = info[i]
+        available.push(key);
+    }
+
+    for(let i = 0; i < data.length; i++) {
+        let [key, value] = data[i]
+        available.push(key);
+    }
+
+    const check_field = required.filter(value => available.includes);
+
+    if (!check_field) {
+        console.log(new Error("The required field (name, version, description, author, and script) aren't present in the file you submitted."))
+        return;
+    } 
+
+    return {available:available, info:owl["Information"],  data:owl["Data"]};
+
 }
 
+let parsed = await load("https://cdn.statically.io/gh/noctisatrae/owl/master/test/files/some-package.toml");
+console.log(parsed);
