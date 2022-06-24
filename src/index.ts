efaed0741bd23f6ae873207c1303e8597020bc72
noctisatrae <pro.hector.kub@gmail.com>
2022-06-24 21:25:01 +0200

    Update...

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import p from "https://esm.sh/phin";
 import { parse } from "https://esm.sh/toml"
 
-const pkg = await p({
-    url:"https://cdn.statically.io/gh/noctisatrae/owl/master/test/files/some-package.toml",
-    parse: "string"
-});
+async function load(url:string) {
+    const pkg = await p({
+        url: url,
+        parse: "string"
+    });
+    
+    const owl = parse(pkg.body);
 
-const owl = parse(pkg.body);
+    // Information
+    const name: string = owl["Information"].name
+    const version: string = owl["Information"].version
+    const description: string = owl["Information"].description
+    const homepage: string = owl["Information"].homepage
+
+    // Data
+    const files: string = owl["Data"].files
+    const script: string = owl["Data"].script
+    const checksum: string = owl["Data"].checksum
+
+    return {info:[name, version, description, homepage], data:[]};
+}
+
