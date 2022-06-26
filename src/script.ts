36582dcf2906a8535e8eca54510101690941f21d
noctisatrae <pro.hector.kub@gmail.com>
2022-06-26 22:08:56 +0200

    Test error handling.

--- src/script.ts
+++ src/script.ts
@@ -3,10 +3,14 @@ import { TerminalSpinner } from "https://deno.land/x/spinners@v1.1.2/mod.ts";
 
 export const script = async (what: string) => {
     const spinner = new TerminalSpinner("Installing everything!\n")
-    
+
     const install = await exec(`bash -c "curl --silent  ${what} | sh -"`);
     
+    console.log(install.status);
+
     if (install.status) {
         spinner.succeed("Done!")
+    } else {
+        spinner.fail("The install script returned an error code. See STDOUT for for more info.")
     }
 }
--- test/files/install.sh
+++ test/files/install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 echo "Installing..."
+exit 1
